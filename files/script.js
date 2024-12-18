a8f9f498d751688f03ccff0fe2d1f284c1c61b99
Alexandr4002 <aold4002@gmail.com>
2024-12-18 05:33:05 +0300

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,12 @@
+new Swiper('.gallery__content', {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: '.gallery__pagination',
+    type: 'fraction'
+  },
+  navigation: {
+    nextEl: '.gallery__right',
+    prevEl: '.gallery__left',
+  },
+});
